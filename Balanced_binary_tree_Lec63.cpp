90c618a39b8f66cbac17d5d78be13b72ebc4ee65
Anirudh singh <rudrasingh62204@gmail.com>
2026-03-08 13:51:15 +0530

    balanced binary tree approach 2(Pair) solved

--- Balanced_binary_tree_Lec63.cpp
+++ Balanced_binary_tree_Lec63.cpp
@@ -1,5 +1,47 @@
 
                       // Que - 3 check balanced binary tree
+// #include<iostream>
+// #include<algorithm>
+// using namespace std;
+// struct Node{
+//     int data;
+//     Node* left;
+//     Node* right;
+// };
+// class solution{
+//     private:
+//     int height(Node* node){
+//         if(node ==NULL){
+//             return 0;
+//         }
+//         int left = height(node -> left);
+//         int right = height(node -> right);
+        
+//         int ans = max(left , right);
+//         return ans;
+//     }
+//   public:
+//   bool isbalanced(Node* node){
+//       if(node==NULL){
+//           return true;
+          
+//       }
+//       bool left = isbalanced(node-> left);
+//       bool right = isbalanced(node-> right);
+//       bool diff = abs(height(node -> left) - height(node -> right)) <=1;
+      
+      
+//       if(left && right && diff){
+//           return true;
+          
+//       }else{
+//           return false;
+//       }
+//   }
+// };   
+
+
+// Approach - 2 pair<int int> ka use karenge jisse height aur diameter ek sath call hoga
 #include<iostream>
 #include<algorithm>
 using namespace std;
@@ -9,41 +51,6 @@ struct Node{
     Node* right;
 };
 class solution{
-    private:
-    int height(Node* node){
-        if(node ==NULL){
-            return 0;
-        }
-        int left = height(node -> left);
-        int right = height(node -> right);
-        
-        int ans = max(left , right);
-        return ans;
-    }
-  public:
-  bool isbalanced(Node* node){
-      if(node==NULL){
-          return true;
-          
-      }
-      bool left = isbalanced(node-> left);
-      bool right = isbalanced(node-> right);
-      bool diff = abs(height(node -> left) - height(node -> right)) <=1;
-      
-      
-      if(left && right && diff){
-          return true;
-          
-      }else{
-          return false;
-      }
-  }
-};   
-
-
-// Approach - 2 pair<int int> ka use karenge jisse height aur diameter ek sath call hoga
-
-class solution{
     public:
     pair<bool ,  int> isBalancedfast(Node* node){
         if(node == NULL){
@@ -52,11 +59,11 @@ class solution{
         }
         pair<int , int> left = isBalancedfast(node ->left);
         pair<int , int> right = isBalancedfast(node -> right);
-        bool leftAns = left.first;
+        bool leftAns = left.first; // first(bool) - subtree balanced hai ya nahi
         bool rightAns = right.first;
-        bool diff = abs(left.second - right.second) <=1;
+        bool diff = abs(left.second - right.second) <=1; // second(int) - subtree ki height
         pair<bool , int> ans;
-        ans.second = max(left.second - right.second)+1;
+        ans.second = max(left.second , right.second)+1;
         if(leftAns && rightAns && diff){
             ans.first = true;
         }else{
